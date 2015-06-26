19571f9bfd441bd8a3cf7a365f04db910fbfde78
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-06-26 22:14:59 +0700

    Update sample for #3

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,7 +2,30 @@
 namespace app\controllers;
 
 use fproject\rest\ActiveController;
+use yii\db\ActiveQuery;
 
 class UserController extends ActiveController{
     public $modelClass = 'app\models\User';
+
+    /**
+     * Condition to find all resources with relations.
+     * Use '@findAllCondition' as the key for client-side condition
+     * @param array $params
+     * @return ActiveQuery
+     */
+    public function findAllCondition($params)
+    {
+        /** @var ActiveQuery $query */
+        $query = \Yii::createObject(ActiveQuery::className(), [$this->modelClass]);
+
+        $query->joinWith([
+            'profile'=> function ($q) {
+                /** @var ActiveQuery $q */
+                $q->select('id,phone');
+            },
+            'department',
+        ],false);
+
+        return $query;
+    }
 }
